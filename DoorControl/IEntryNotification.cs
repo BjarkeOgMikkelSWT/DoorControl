9dc114a13c4865fc703cb883284a2527df17ec8a
Bjarke <201805703@post.au.dk>
2021-02-24 10:30:01 +0100

    IAlarm and INotify added

---
+++ DoorControl/IEntryNotification.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoorControl
+{
+    interface IEntryNotification
+    {
+        void NotifyEntryGranted();
+        void NotifyEntryDenied();
+    }
+}
