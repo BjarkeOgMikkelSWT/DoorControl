1a5ae4692db7a262cb4086b9200203f9ac379ab1
Mikkel Kirkegaard <fisk9889@gmail.com>
2021-02-24 10:34:06 +0100

    Added MockDoor to the test project

---
+++ DoorControl.Test.Unit/MockDoor.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+
+namespace DoorControl.Test.Unit
+{
+    public class MockDoor : IDoor
+    {
+        public void Close()
+        {
+        }
+
+        public void Open()
+        {
+        }
+    }
+}
